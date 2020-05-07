3cec187e55c1a5ce71aad63dec842484f12fb8dd
Yunsheng Luo <38714962+LuckyOne09@users.noreply.github.com>
2020-05-08 06:56:41 +0800

    Update ShowPreferencesAction.java

--- src/main/java/org/jabref/gui/preferences/ShowPreferencesAction.java
+++ src/main/java/org/jabref/gui/preferences/ShowPreferencesAction.java
@@ -3,19 +3,34 @@ package org.jabref.gui.preferences;
 import org.jabref.gui.JabRefFrame;
 import org.jabref.gui.actions.SimpleCommand;
 import org.jabref.gui.util.TaskExecutor;
+import org.jabref.gui.util.ThemeLoader;
+import org.jabref.preferences.JabRefPreferences;
 
 public class ShowPreferencesAction extends SimpleCommand {
 
     private final JabRefFrame jabRefFrame;
     private final TaskExecutor taskExecutor;
+    private final ThemeLoader themeLoader;
+    private final JabRefPreferences prefs;
 
-    public ShowPreferencesAction(JabRefFrame jabRefFrame, TaskExecutor taskExecutor) {
+    public ShowPreferencesAction(JabRefFrame jabRefFrame, TaskExecutor taskExecutor, ThemeLoader themeLoader, JabRefPreferences prefs) {
         this.jabRefFrame = jabRefFrame;
         this.taskExecutor = taskExecutor;
+        this.themeLoader = themeLoader;
+        this.prefs = prefs;
     }
 
+    /**
+     * Execute the action showing {@link PreferencesDialogView}
+     */
     @Override
     public void execute() {
-        new PreferencesDialogView(jabRefFrame).show();
+        PreferencesDialogView preferencesDialogView = new PreferencesDialogView(jabRefFrame);
+        preferencesDialogView.show();
+        for (PreferencesTab tab: preferencesDialogView.getPreferenceTabList().getItems()) {
+            if (tab.getBuilder().getScene() != null) {
+                this.themeLoader.installCss(tab.getBuilder().getScene(), this.prefs);
+            }
+        }
     }
 }
